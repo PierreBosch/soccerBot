12276b8dae9e6c38fd3bf1f30ada6b2c6d9a459f
Pierre <pierre.mota@afya.com.br>
2024-07-23 10:56:44 -0300

    fix: corrigir nome do jogador ao pedir cardápio

--- src/commands/get-food-menu.js
+++ src/commands/get-food-menu.js
@@ -1,13 +1,14 @@
 const getRandomFunSentence = require('../util/get-random-fun-sentence');
 const funnyPhrasesOnFoodMenu = require('../sentences/funny-on-food-menu');
 const getFoodMenuTemplate = require('../templates/get-food-menu-template');
+const getPlayerName = require('../util/extract-first-and-last-name');
 
 async function getFoodMenu(message, client) {
   const playerName = message.sender.pushname;
   const sender = message.from;
 
   await client.sendText(sender, getFoodMenuTemplate())
-  const firstName = getPlayerName(playerName, true);
+  const firstName = getPlayer(playerName, true);
 
   await client.sendText(sender, getRandomFunSentence(funnyPhrasesOnFoodMenu, firstName))
 }
