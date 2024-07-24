29f7b2138072dde1d4a6f495210da2712a854e03
Pierre <pierre.mota@afya.com.br>
2024-07-24 15:50:26 -0300

    fix: corrige erro de permissao na lista de devedores

--- src/commands/get-debtors-list.js
+++ src/commands/get-debtors-list.js
@@ -2,9 +2,10 @@ const isAdmin = require("../permissions");
 const getDebtorsListTemplate = require("../templates/get-debtors-list-template");
 
 async function getDebtorsList(message, client, byPassAdmin = false) {
-  const sender = message.sender.id;
+  const adminId = message.sender.id;
+  const sender = message.from;
 
-  if(!isAdmin(sender) && !byPassAdmin)  {
+  if(!isAdmin(adminId) && !byPassAdmin)  {
     return client.sendText(sender, 'Somente admins podem mostrar a lista de pagantes');
   }
 
