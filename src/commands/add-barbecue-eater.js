2f654bdd899d085a9562d16c482919485819868e
Pierre <pierre.mota@afya.com.br>
2024-07-26 09:28:53 -0300

    feat: cria exception para adicionar mais de uma vez a mesma pessoa no churrasco

--- src/commands/add-barbecue-eater.js
+++ src/commands/add-barbecue-eater.js
@@ -5,6 +5,9 @@ const getRandomFunSentence = require('../util/get-random-fun-sentence');
 const funnyPhrasesOnAddBarbecue = require('../sentences/funny-on-add-barbecue');
 const addBarbecueEaterService = require('../http/add-to-barbecue');
 const isAdmin = require('../permissions');
+const getBarbecueEaters = require('./get-barbecue-eaters');
+
+const barbecueEaterAlreadyExistsException = "Você já está na lista do churrasco, seu nome não pode ser adicionado mais de uma vez"
 
 async function addBarbecueEater(message, client) {
   try {
@@ -16,6 +19,14 @@ async function addBarbecueEater(message, client) {
       const stayForCoke = !!addCoke 
 
       const [,barbecueEaterGuest] = message.body.split("|")
+
+      const barbecueEaters = await getBarbecueEaters();
+
+      const barbecueEaterExists = barbecueEaters.find(barbecueEater => barbecueEater.name === playerName || barbecueEater.name === barbecueEaterGuest)
+
+      if(barbecueEaterExists) {
+        return await client.sendText(sender, barbecueEaterAlreadyExistsException)
+      }
 
       if(isAdmin(senderId) && !!barbecueEaterGuest) {
         await addBarbecueEaterService(getPlayerName(barbecueEaterGuest), stayForCoke)
