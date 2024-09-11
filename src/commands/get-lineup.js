bee5cc551de717d42ae44a5f916d3df29b9d795b
Pierre <pierre.mota@afya.com.br>
2024-09-10 21:20:18 -0300

    feat: adiciona escalacao

--- src/commands/get-lineup.js
+++ src/commands/get-lineup.js
@@ -1,10 +1,10 @@
 async function getLineup(message, client) {
   const sender = message.from;
-  
-  const imageUrl = 'https://i.ibb.co/Wvd51Sf/Escala-o-26-08-24.png'
-  
-  if(imageUrl !== null) {
-    await client.sendImage(sender, imageUrl)              
+
+  const imageUrl = 'https://i.ibb.co/3FrNBCb/Escala-o-26-08-24-2.png'
+
+  if (imageUrl !== null) {
+    await client.sendImage(sender, imageUrl)
   } else {
     await client.sendText(sender, 'Calma calabreso! A escalação ainda está sendo montada.')
   }
