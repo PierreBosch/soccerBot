394319a28811113259e23df1eea414f5c34c7467
Pierre <pierre.mota@afya.com.br>
2024-09-11 10:18:36 -0300

    feat: criar escalacao

--- src/commands/get-lineup.js
+++ src/commands/get-lineup.js
@@ -1,7 +1,7 @@
 async function getLineup(message, client) {
   const sender = message.from;
 
-  const imageUrl = 'https://i.ibb.co/3FrNBCb/Escala-o-26-08-24-2.png'
+  const imageUrl = 'https://i.ibb.co/8b7tKk3/escalacao-11-09.png'
 
   if (imageUrl !== null) {
     await client.sendImage(sender, imageUrl)
