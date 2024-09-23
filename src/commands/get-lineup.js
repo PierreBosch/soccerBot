4ba9c50239cddb04fa3e6d71b0dcb132690af2be
Pierre <pierre.mota@afya.com.br>
2024-09-23 20:37:58 -0300

    feat: cria escalaçao

--- src/commands/get-lineup.js
+++ src/commands/get-lineup.js
@@ -1,7 +1,7 @@
 async function getLineup(message, client) {
   const sender = message.from;
 
-  const imageUrl = 'https://i.ibb.co/ZgfkmyZ/Escalac-a-o-26-08-24-3.png'
+  const imageUrl = 'https://i.imghippo.com/files/GEERV1727134629.png'
 
   try {
     await client.sendImage(sender, imageUrl)
