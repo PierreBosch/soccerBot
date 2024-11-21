0ea10fd9e1e5fe6c13ed8ed4cdead30e7f153533
root <root@vmi2285301.contaboserver.net>
2024-11-20 19:21:55 -0600

    feat: muda line-up

--- src/commands/get-lineup.js
+++ src/commands/get-lineup.js
@@ -1,7 +1,7 @@
 async function getLineup(message, client) {
   const sender = message.from;
 
-  const imageUrl = 'https://i.ibb.co/1qmq8V1/Escalacao-8.png'
+  const imageUrl = 'https://i.ibb.co/PDcRLCN/Escalacao-13.png'
 
   try {
     await client.sendImage(sender, imageUrl)
